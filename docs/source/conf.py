f5d29a371455fd369a00b8b3a1b7d3009406d434
Alexandros Korkovelos <alekor@UG.KTH.SE>
2019-05-08 17:30:40 +0100

    Changes in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,21 +12,21 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'gons_test'
-copyright = '2019, HTK'
-author = 'HTK'
+project = 'The GEP User Guide'
+copyright = '2019, The Global Electrification Platform'
+author = 'KTH dESA'
 
-import gons_test
 # The short X.Y version
-version = release = gons_test.__version__
+version = 'Version 2019.0'
 # The full version, including alpha/beta/rc tags
+release = '01-06-2019'
 
 
 # -- General configuration ---------------------------------------------------
@@ -39,27 +39,16 @@ version = release = gons_test.__version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'm2r',
-
 ]
-autodoc_mock_imports = ["numpy", "scikit-image", "rasterio", "geopandas", "Rtree",
-                        "affine", "descartes", "Pillow", "pyproj", "pytz", "IPython",
-                        "pandas", "Shapely", "scipy", "Fiona", "skimage", "shapely",
-                        "fiona"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['ntemplates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
@@ -73,11 +62,11 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# This pattern also affects html_static_path and html_extra_path .
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -85,7 +74,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -96,7 +85,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['nstatic']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -112,7 +101,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'gons_testdoc'
+htmlhelp_basename = 'TheGEPUserManualdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -139,8 +128,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'gons_test.tex', 'gons_test Documentation',
-     'HTK', 'manual'),
+    (master_doc, 'TheGEPUserManual.tex', 'The GEP User Guide',
+     'KTH dESA', 'manual'),
 ]
 
 
@@ -149,7 +138,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'gons_test', 'gons_test Documentation',
+    (master_doc, 'thegepusermanual', 'The GEP User Guide',
      [author], 1)
 ]
 
@@ -160,28 +149,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'gons_test', 'gons_test Documentation',
-     author, 'gons_test', 'One line description of project.',
+    (master_doc, 'TheGEPUserManual', 'The GEP User Guide',
+     author, 'TheGEPUserManual', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-# -- Options for Epub output -------------------------------------------------
-
-# Bibliographic Dublin Core info.
-epub_title = project
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#
-# epub_identifier = ''
-
-# A unique identification for the text.
-#
-# epub_uid = ''
-
-# A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
-
-
-# -- Extension configuration -------------------------------------------------
